c1693c535660a9ec967ce5908964c5aa06864186
DetectiveSh4rk <31936882+DetectiveSh4rk@users.noreply.github.com>
2022-01-20 13:34:26 -0500

    backround image removed on this branch for testing

---
+++ src/containers/Greet/index.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { useHistory } from 'react-router-dom'
+import { Background, MainContainer, ButtonContainer, GreetHead, GreetDesc } from './styles'
+import Button from '../../components/ButtonComponent'
+
+const Greet = () => {
+  const history = useHistory();
+  const goTo = (string) => {
+    history.push("/".concat(string))
+  }
+    return(
+        <Background>
+          <MainContainer>
+            <GreetHead>Bookify</GreetHead>
+            <GreetDesc>
+              Welcome to <span>Bookify</span>! Your new home for book logging, recommendations and sharing with friends! 
+              <br></br>
+              <u>Features:</u>
+              <ul>
+                <li>
+                  Search for books by author or title
+                </li>
+                <li>
+                  Log books you've read or are looking to read
+                </li>
+                <li>
+                  Recommend books to friends and see their recommendations for you
+                </li>
+                <li>
+                  and more to come soon!
+                </li>
+              </ul>
+            </GreetDesc>
+          </MainContainer>
+          <ButtonContainer>
+          <form
+            onSubmit={e => {
+              goTo('login')
+            }}
+          >
+            <Button content = "Click Here to get started!"></Button>
+          </form>
+          </ButtonContainer>
+        </Background>
+    )
+
+}
+
+export default Greet
+
+/*
+  <div>Temporary, Andrew will handle later</div>
+  <button onClick = {() => goTo("login")}>go to login</button>
+  <button onClick = {() => goTo("register")}>go to register</button>
+*/
---
+++ src/containers/Greet/styles.js
@@ -0,0 +1,66 @@
+import styled from 'styled-components'
+//import img from '../../img/greetBackground.jpg'
+//background-image:url(${img});
+export const Background = styled.div`
+  
+  margin: 0;
+  background-size:cover;
+  background-position:center;
+  height:100vh;
+  width: 100%;
+  display:flex;
+  flex-direction:column;
+  justify-content: center;
+  align-items:center;
+  gap: 5vh;
+`
+
+export const MainContainer = styled.div`
+  height: 40vh;
+  width: 50%;
+  background-color: ${props => props.theme.colors.lblue};
+  border: 1px solid ${props => props.theme.colors.lightheader};
+  border-radius: 10px;
+  `
+
+export const GreetHead = styled.div`
+  width: 80%;
+  margin-left: 10%;
+  padding: 1.5vh 0%;
+  text-align: center;
+  font-size: 36px;
+  font-weight: bolder;
+  color: ${props => props.theme.colors.brown};
+  -webkit-text-stroke-width: 1px;
+  -webkit-text-stroke-color: black;
+  border-bottom: 1px solid ${props => props.theme.colors.brown};
+  `
+
+export const GreetDesc = styled.div`
+  margin-top: 2vh;
+  text-align: center;
+  span{
+    font-weight: bolder;
+  }
+  ul{
+    list-style: none;
+  }
+  `
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  gap: 20px;
+  height: 10vh;
+  width: 50%;
+  form{
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    button{
+    width: 60%;
+    }
+  } 
+  `
