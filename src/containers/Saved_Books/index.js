1fa3d6212d9e9a989ee56ec77ff757676a9e958b
Julianna Zhao <juliannazhao@college.harvard.edu>
2022-01-20 22:44:02 -0500

    new home page

--- src/containers/Saved_Books/index.js
+++ src/containers/Saved_Books/index.js
@@ -1,33 +1,38 @@
 import React from 'react'
 import { useQuery } from '@apollo/react-hooks'
+import { useHistory } from 'react-router-dom'
+import jwt_decode from 'jwt-decode'
 import { Container, Text } from './styles'
+import CardComponent from '../../components/CardComponent'
 import { GET_SAVED_BOOKS_BY_USER } from './graphql'
 import Saved_BooksTable from '../../components/Saved_BooksTable'
-import { useHistory } from 'react-router-dom'
-import jwt_decode from "jwt-decode"
+import NavComponent from '../../components/NavComponent'
 
 
 const Saved_Books = () => {
-    const history = useHistory()
-    const token = localStorage.getItem('token')
-    if(!token){
-        history.push('/login')
-    }
-    const ID = jwt_decode(token).id
-    const {loading, error, data} = useQuery(GET_SAVED_BOOKS_BY_USER, {
-        variables: {userID: ID}})
-    if (loading) 
-    {
-        return <Container><Text>Loading...</Text></Container>
-    } else if (error)
-    {
-        return <Container><Text>Error!</Text></Container>
-    }
-    return (
-        <Container>
-            <Saved_BooksTable data={data} />
-        </Container>
-    )
+  const history = useHistory()
+  const token = localStorage.getItem('token')
+  if (!token) {
+    history.push('/login')
+  }
+  const ID = jwt_decode(token).id
+  const { loading, error, data } = useQuery(GET_SAVED_BOOKS_BY_USER, { variables: { userID: ID } })
+  if (loading) {
+    return <Container><Text>Loading...</Text></Container>
+  } if (error) {
+    return <Container><Text>Error!</Text></Container>
+  }
+  const content = (
+    <>
+      <Saved_BooksTable data={data} />
+    </>
+  )
+  return (
+    <>
+      <NavComponent />
+      <CardComponent content = {content} />
+    </>
+  )
 }
 
 export default Saved_Books
