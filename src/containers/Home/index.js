9e7fa62280b575ca2fd2f9e1fd4a953000555aff
Julianna Zhao <juliannazhao@college.harvard.edu>
2022-01-20 22:45:57 -0500

    new home page

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,18 +1,10 @@
 import React from 'react'
-import CardComponent from '../../components/CardComponent'
-import NavComponent from '../../components/NavComponent'
-import Search from '../../components/Search v1'
+import Saved_Books from '../Saved_Books'
 
 const Home = () => {
-  const content = (
-    <>
-      <Search header="Book Search" subHeader="Search for books by title or author!" desc="e.g. Harry Potter or Charles Dickens"/>
-    </>
-  )
   return (
     <> 
-      <NavComponent/>
-        <CardComponent content = {content} />
+      <Saved_Books />
     </>
   )
 }
